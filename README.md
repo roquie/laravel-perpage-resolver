770ed289dd196ad85393e521f18a2b3ae78cc8d2
roquie <roquie0@gmail.com>
2017-02-15 16:33:19 +0300

    Docs & others.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-Coming soon docs are available.
+Laravel PerPage Resolver
+========================
+
+This package add the same method `perPageResolver` like as `currentPageResolver`. Now,
+`?per_page=100` parameter have a globally access for all models (like `?page=2`). 
+
+## How to use
+
+For the beginning, install composer package: <br>
+```php
+    composer require roquie/laravel-perpage-resolver
+```
+
+Second, you should extends package Model class:
+
+```php
+    class User extends \Roquie\LaravelPerPageResolver\Model
+    {
+    // ...
+```
+
+Third, apply a service provider it's here:
+
+```php
+    // config/app.php, the providers array
+    // ...
+    Roquie\LaravelPerPageResolver\PerPageResolverServiceProvider::class,
+```
+
+Run it!
+
+## Tests
+
+PR are welcome ;)
+
+## License
+ 
+MIT 
--- src/Builder.php
+++ src/Builder.php
@@ -15,5 +15,37 @@ namespace Roquie\LaravelPerPageResolver;
  */
 class Builder extends \Illuminate\Database\Eloquent\Builder
 {
-    use PerPageResolverTrait;
+    /**
+     * Paginate the given query.
+     *
+     * @param  int  $perPage
+     * @param  array  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+    {
+        $perPage = $perPage ?: Paginator::resolvePerPage();
+
+        return parent::paginate($perPage, $columns, $pageName, $page);
+    }
+
+    /**
+     * Paginate the given query into a simple paginator.
+     *
+     * @param  int  $perPage
+     * @param  array  $columns
+     * @param  string  $pageName
+     * @param  int|null  $page
+     * @return \Illuminate\Contracts\Pagination\Paginator
+     */
+    public function simplePaginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+    {
+        $perPage = $perPage ?: Paginator::resolvePerPage();
+
+        return parent::simplePaginate($perPage, $columns, $pageName, $page);
+    }
 }
