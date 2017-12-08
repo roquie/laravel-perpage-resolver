f2fd7f12780ae31da2968eb78a536c9eb696e483
Max <Roquie0@gmail.com>
2017-12-08 17:21:51 +0300

    Update PerPageResolverTrait.php

--- src/PerPageResolverTrait.php
+++ src/PerPageResolverTrait.php
@@ -15,5 +15,14 @@ namespace Roquie\LaravelPerPageResolver;
  */
 trait PerPageResolverTrait
 {
-    // @todo: ...
+    /**
+     * Create a new Eloquent query builder for the model.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public function newEloquentBuilder($query)
+    {
+        return new Builder($query);
+    }
 }
